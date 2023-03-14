038b9fe42c731eab5c7cd4a799f32f2029a81cf2
pierre <pspam@net-c.com>
2023-03-14 10:56:58 +0100

    k8s install functional

--- k8s/set_training_env/multiple_nodes/config-post-install/master_init.sh
+++ k8s/set_training_env/multiple_nodes/config-post-install/master_init.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-sudo kubeadm init --apiserver-advertise-address=192.168.205.100 --pod-network-cidr=192.168.205.100/24 --upload-certs
+sudo kubeadm init --apiserver-advertise-address=192.168.205.100 --pod-network-cidr=192.168.0.0/16 --upload-certs
 mkdir -p $HOME/.kube                                                                                                              
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config                                                                          
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/tigera-operator.yaml
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/custom-resources.yaml
-
+kubectl create -f config-post-install/custom-resources.yaml
